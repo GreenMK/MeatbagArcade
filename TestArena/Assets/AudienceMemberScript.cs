9103981f902a8168f04c40b22fb135e5611b62ba
GreenMK <mkerkow1@gmail.com>
2015-10-12 11:16:37 +1000

    MK Arena

---
+++ TestArena/Assets/AudienceMemberScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudienceMemberScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.position = new Vector3 (transform.position.x + Mathf.Sin(Time.time * speed),transform.position.y,transform.position.z);
+	}
+}
